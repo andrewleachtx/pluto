78531e3cc1d4cac250105e8044d39eb1f66d1e88
andrewleachtx <andrewleach38@gmail.com>
2026-09-06 20:58:37 -0700

    Add window, close, working on VBO

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+
+#include <glad/glad.h>
+
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+const float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
 int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "pluto", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to make GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress) ) {
+        std::cout << "Failed to init GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // render loop: render, swap, poll
+    while (!glfwWindowShouldClose(window)) {
+        // render
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // swap buffers
+        glfwSwapBuffers(window);
+        
+        // poll then handle inputs
+        glfwPollEvents();
+        process_input(window);
+    }
+
+    glfwTerminate();
+
     return 0;
 }
